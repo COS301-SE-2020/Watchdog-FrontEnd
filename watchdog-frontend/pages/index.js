a7426ef9bd40009507a14f281f4be19e723f30a3
Luqmaan Badat <luqmaan.badat@tuks.co.za>
2020-06-22 17:46:09 +0000

    Squashed commit of the following
    commit 170bcb28 
    Author: Luqmaan <luqmaan.badat@gmail.com> 
    Date: Mon Jun 22 2020 19:44:45 GMT+0200 (South Africa Standard Time) 
    
        fix merge conflict

--- watchdog-frontend/pages/index.js
+++ watchdog-frontend/pages/index.js
@@ -2,12 +2,6 @@ import Head from 'next/head'
 import Login from '../components/Login'
 import React, { Component } from 'react';
 
-function Home(){
-  return (
-    <div>
-      <Header />
-      <Login />
-
 class Home extends Component{
   constructor(){
     super();
