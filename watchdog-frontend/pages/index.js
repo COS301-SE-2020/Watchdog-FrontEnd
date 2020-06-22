a8d6afa52c28288b693d848a11261084d9a201b1
Luqmaan <luqmaan.badat@gmail.com>
2020-06-22 18:10:57 +0200

    Update login page

--- watchdog-frontend/pages/index.js
+++ watchdog-frontend/pages/index.js
@@ -1,16 +1,37 @@
 import Head from 'next/head'
 import Login from '../components/Login'
-import Header from "../components/Header"
+import React, { Component } from 'react';
 
-function Home(){
-  return (
-    <div>
-      <Header />
-      <Login />
-    </div>
-    
-  )
 
+class Home extends Component{
+  constructor(){
+    super();
+  }
+
+  render(){
+    const styling = {
+      "backgroundColor": "black"
+    }
+    const defaulTitle = "Watchdog System"
+    return (
+      <div  >
+        <Head>
+          <style>{'body { background-color: black; }'}</style>
+          <meta charSet="UTF-8"/>
+          <title>{this.props.title || defaulTitle}</title>
+          <link rel = "stylesheet"
+                type = "text/css"
+                href = "/style.css"/>
+        
+        </Head>
+          <Login />
+        
+      </div>
+      
+    )
+  }
+  
+  
 }
 
 export default Home
