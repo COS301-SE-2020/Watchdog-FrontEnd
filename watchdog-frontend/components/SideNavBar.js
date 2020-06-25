ebbadd4eb5ab06abe21e3c6e6009cb05883b1f1b
Aboobacker Kharbai <abubakrkharbai1@gmail.com>
2020-06-25 11:44:07 +0000

    Squashed commit of the following
    commit 86d40dcb 
    Author: Abubakr <abubakrkharbai1@gmail.com> 
    Date: Thu Jun 25 2020 13:41:54 GMT+0200 (South Africa Standard Time) 
    
        NavBar changed

--- watchdog-frontend/components/SideNavBar.js
+++ watchdog-frontend/components/SideNavBar.js
@@ -1,73 +1,57 @@
 import React, {Component} from 'react'
-import { Toggle, Sidenav, Nav, Icon, Dropdown   } from 'rsuite'
+import { Toggle, Sidenav, Nav, Icon, Dropdown, IconButton   } from 'rsuite'
 
 
 class SideNavBar extends Component {
     constructor() {
         super();
         this.state = {
-          expanded: true,
-          activeKey: '1'
+          expanded: false,
+          // activeKey: '1'
         };
         this.handleToggle = this.handleToggle.bind(this);
-        this.handleSelect = this.handleSelect.bind(this);
+        // this.handleSelect = this.handleSelect.bind(this);
       }
       handleToggle() {
         this.setState({
           expanded: !this.state.expanded
         });
       }
-      handleSelect(eventKey) {
-        this.setState({
-          activeKey: eventKey
-        });
-      }
+      // handleSelect(eventKey) {
+      //   this.setState({
+      //     activeKey: eventKey
+      //   });
+      // }
       render() {
         const { expanded } = this.state;
     
         return (
           <div style={{ width: 250 }}>
-            <Toggle onChange={this.handleToggle} checked={expanded} />
+            <IconButton size="lg" onClick={this.handleToggle} checked={expanded}color="black" icon={<Icon icon="list" />}/>
             <hr />
             <Sidenav
               expanded={expanded}
-              defaultOpenKeys={['3', '4']}
               activeKey={this.state.activeKey}
-              onSelect={this.handleSelect}
+              // onSelect={this.handleSelect}
             >
               <Sidenav.Body>
                 <Nav>
-                  <Nav.Item eventKey="1" icon={<Icon icon="dashboard" />}>
-                    Dashboard
+                  <Nav.Item eventKey="1" href='/Home' icon={<Icon icon="home" />}>
+                    Home
                   </Nav.Item>
-                  <Nav.Item eventKey="2" icon={<Icon icon="group" />}>
-                    User Group
+                  <Nav.Item eventKey="2" href='/liveVideo' icon={<Icon icon="video-camera" />}>
+                    View Live Video
                   </Nav.Item>
-                  <Dropdown
-                    placement="rightStart"
-                    eventKey="3"
-                    title="Advanced"
-                    icon={<Icon icon="magic" />}
-                  >
-                    <Dropdown.Item eventKey="3-1">Geo</Dropdown.Item>
-                    <Dropdown.Item eventKey="3-2">Devices</Dropdown.Item>
-                    <Dropdown.Item eventKey="3-3">Loyalty</Dropdown.Item>
-                    <Dropdown.Item eventKey="3-4">Visit Depth</Dropdown.Item>
-                  </Dropdown>
-                  <Dropdown
-                    placement="rightStart"
-                    eventKey="4"
-                    title="Settings"
-                    icon={<Icon icon="gear-circle" />}
-                  >
-                    <Dropdown.Item eventKey="4-1">Applications</Dropdown.Item>
-                    <Dropdown.Item eventKey="4-2">Channels</Dropdown.Item>
-                    <Dropdown.Item eventKey="4-3">Versions</Dropdown.Item>
-                    <Dropdown.Menu eventKey="4-5" title="Custom Action">
-                      <Dropdown.Item eventKey="4-5-1">Action Name</Dropdown.Item>
-                      <Dropdown.Item eventKey="4-5-2">Action Params</Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
+                  <Nav.Item eventKey="3" href='/SavedVideo' icon={<Icon icon="logo-video" />}>
+                    View Saved Video
+                  </Nav.Item>
+                  <Nav.Item eventKey="4" href='/Profile' icon={<Icon icon="profile" />}>
+                    Profile
+                  </Nav.Item>
+                  <Nav.Item eventKey="5" href='/index' icon={<Icon icon="sign-out" />}>
+                    Log Out
+                  </Nav.Item>
+                 
                 </Nav>
               </Sidenav.Body>
             </Sidenav>
