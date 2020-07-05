e04430296b71b53fc33c89e65941d37fd7349f7d
Luqmaan <luqmaan.badat@gmail.com>
2020-07-05 16:24:29 +0200

    Hot fix:error when merging

--- watchdog-frontend/components/SideNavBar.js
+++ watchdog-frontend/components/SideNavBar.js
@@ -10,7 +10,7 @@ class SideNavBar extends Component {
         super(props);
         //console.log(this.props)
         this.state = {
-          expanded: false,
+          expanded: true,
           activeKey: '1',
           activeKey: this.props.defaultKeyVal
         };
