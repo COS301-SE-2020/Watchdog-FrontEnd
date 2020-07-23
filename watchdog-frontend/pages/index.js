4f194efaa71c4e2999defa82910408dea3580dec
Luqmaan <luqmaan.badat@gmail.com>
2020-07-23 16:53:51 +0200

    hot fix to fix build error

--- watchdog-frontend/pages/index.js
+++ watchdog-frontend/pages/index.js
@@ -112,43 +112,7 @@ class Index extends Component{
   
 }
 
-// function Index (props){
-//   //const router = useRouter()
-//   useEffect(() => {
-//       Hub.listen('auth', (data) => {
-//         const { payload } = data
-//         //console.log('A new auth event has happened: ', data)
-//          if (payload.event === 'signIn') {
-//            //console.log('a user has signed in!')
-//            Router.push("/Home")
-//          }
-//          if (payload.event === 'signOut') {
-//            //console.log('a user has signed out!')
-//          }
-//       })
-//     }, [])
-//     Auth.currentAuthenticatedUser()
-//     .then(data => Router.push("/Home"))
-//     .catch(error => {})
-//     return (
-      
-//       <div  >
-//         <Head>
-//           <style>{'body { background-color: black; }'}</style>
-//           <meta charSet="UTF-8"/>
-//           <title>{props.title || defaulTitle}</title>
-//           <link rel = "stylesheet"
-//                 type = "text/css"
-//                 href = "/style.css"/>
-        
-//         </Head>
-//           <Login />
-        
-//       </div>
-      
-//     )
-  
-// }
+
 
 
 
