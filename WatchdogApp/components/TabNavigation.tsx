4e68f1a24b4e216674c1e1776496f2d850e5822a
Luqmaan <luqmaan.badat@gmail.com>
2020-08-03 13:26:57 +0200

    Add icons to tab navigation

--- WatchdogApp/components/TabNavigation.tsx
+++ WatchdogApp/components/TabNavigation.tsx
@@ -1,27 +1,47 @@
 import React, {Component} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createBottomTabNavigator  } from '@react-navigation/bottom-tabs'
 import DashboardTab from './Dashboard'
 import LiveTab from './Live'
 import RecordingsTab from './Recordings'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { NavigationContainer } from '@react-navigation/native'
+import { Ionicons } from '@expo/vector-icons'
 
 
 
 const Tab = createBottomTabNavigator();
-
 class TabNavigation extends Component{
     constructor(props: any){
         super(props)
     }
 
     render(){
+
         return(
             <SafeAreaProvider>
                 
-                    <Tab.Navigator>
-                        <Tab.Screen name="Recordings" component={RecordingsTab} />
+                    <Tab.Navigator
+                        screenOptions={(route: any)=>({
+                            tabBarIcon: ({ focused, color, size }) => {
+                                let iconName =''
+                                
+                    
+                                if (route.route.name === 'Dashboard') {
+                                  iconName = focused
+                                    ? 'ios-information-circle'
+                                    : 'ios-information-circle-outline'
+                                } else if (route.route.name === 'Recordings') {
+                                  iconName = focused ? 'ios-recording' : 'ios-recording'
+                                }else if(route.route.name === 'Live'){
+                                    iconName = focused ? 'videocam-outline' : 'videocam-outline'
+                                }
+                            
+                                // You can return any component that you like here!
+                                return <Ionicons name={iconName} size={size} color={color} />;
+                              },
+                        })}
+                    >
+                        <Tab.Screen  name="Recordings" component={RecordingsTab} />
                         <Tab.Screen name="Dashboard" component={DashboardTab} />
                         <Tab.Screen name="Live" component={LiveTab} />                        
                     </Tab.Navigator>
