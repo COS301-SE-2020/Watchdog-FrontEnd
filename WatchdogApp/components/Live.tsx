65adfab951f0c79d5df8b60e27f260607d922a01
Rishi <34397126+urishiraval@users.noreply.github.com>
2020-08-03 12:04:01 +0200

    Changed Filenames to follow convertions

---
+++ WatchdogApp/components/Live.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class LiveTab extends Component {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>This is the LIVE Page</Text>
+            </View>
+        );
+    }
+}
+
+export default LiveTab;
