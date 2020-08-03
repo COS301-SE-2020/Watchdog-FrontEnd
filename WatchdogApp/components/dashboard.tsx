b60dabf6b3b0ade61e9af4c6d8cd63990c42d02c
Rishi <34397126+urishiraval@users.noreply.github.com>
2020-08-03 11:57:04 +0200

    Created Core Components (Tabs)

---
+++ WatchdogApp/components/dashboard.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class DashboardTab extends Component {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>This is the DASHBOARD Page</Text>
+            </View>
+        );
+    }
+}
+
+export default DashboardTab;
---
+++ WatchdogApp/components/recordings.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class RecordingsTab extends Component {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>This is the RECORDINGS Page</Text>
+            </View>
+        );
+    }
+}
+
+export default RecordingsTab;
