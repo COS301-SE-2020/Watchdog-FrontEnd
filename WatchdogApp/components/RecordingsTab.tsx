2571a3de65801dc7cc1bdafb0dbce4be3d5b29bd
Rishi <34397126+urishiraval@users.noreply.github.com>
2020-08-08 18:16:41 +0200

    Converted Card to Kitty Component. Added Video component

--- WatchdogApp/components/RecordingsTab.tsx
+++ WatchdogApp/components/RecordingsTab.tsx
@@ -1,19 +1,17 @@
 import React, { Component } from "react"
-import { Text, View, FlatList } from "react-native"
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Card, Avatar } from 'react-native-elements'
-import HeaderBar from './HeaderBar'
+import { StyleSheet } from "react-native"
+import { Video } from 'expo-av'
 import moment from 'moment'
 import CustomTab from "./CustomTab"
-import { Layout } from "@ui-kitten/components"
+import { Card, List, Layout, Avatar, Text } from "@ui-kitten/components"
 
 //Dynamically create dummy data
-const dummyData = Array.from({ length: 10 }, (_, index) => (
+const dummyData = Array.from({ length: 1 }, (_, index) => (
     {
-        key : index,
+        key: index,
         path_in_s3: "somepath",
         location: `Location ${index}`,
-        timestamp: moment.unix(1593568800 + index * 24 * 60 * 60).format("dddd, MMMM Do YYYY, hh:mm a")
+        timestamp: moment.unix(1593568800 + index * 24 * 60 * 60).format("ddd, MM/DD/YY, hh:mm a")
     }
 ))
 
@@ -28,42 +26,64 @@ class RecordingsTab extends Component {
     }
 
     render() {
-        var renderVideo = (obj: any) => (
-            <Card key={obj.index} image={require('../assets/intruder.jpg')}>
-                <View style={{
-                    flexDirection: "row",
-                    padding: 10,
-                    margin: 10
-                }}>
-                    <View style={{ paddingRight: 20 }}>
-                        <Avatar
-                            rounded
-                            size="small"
-                            source={require('../assets/thief.png')}
-                        />
-                    </View>
-                    <View>
-                        <Text style={{ fontWeight: 'bold', fontSize: 20 }}>{obj.item.location}</Text>
-                        <Text>{obj.item.timestamp}</Text>
-                    </View>
-                </View>
-            </Card>
-        )
+        const videos = [...this.state.videos]
 
-        var videos = [...this.state.videos]
+        const renderVideoHeader = (headerProps, info) => (
+            <Layout style={{ flex: 1, flexDirection: 'row', padding: 20 }}>
+                <Layout>
+                    <Avatar source={require('../assets/thief.png')} style={{ marginRight: 20 }} />
+                </Layout>
+                <Layout>
+                    <Text category='h6'>{info.item.timestamp}</Text>
+                </Layout>
+            </Layout>
+        )
+        const renderVideoFooter = (footerProps, info) => (
+            <Text category="label" style={{ padding: 20 }}>{info.item.location.toUpperCase()}</Text>
+        )
+        const renderVideo = (info) => (
+            <React.Fragment>
+                <Card
+                    style={styles.item}
+                    status='basic'
+                    header={headerProps => renderVideoHeader(headerProps, info)}
+                    footer={footerProps => renderVideoFooter(footerProps, info)}>
+
+                    <Video
+                        source={{ uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' }}
+                        isMuted={false}
+                        resizeMode="contain"
+                        useNativeControls
+                        style={{ width: 300, height: 300 }}
+                    />
+
+                </Card>
+            </React.Fragment>
+
+        )
 
         return (
             <CustomTab
                 title="Recordings"
                 tabContent={
-                        <Layout>
-                            <FlatList  data={videos} renderItem={renderVideo} />
-                        </Layout>
+                    <List style={styles.container} contentContainerStyle={styles.contentContainer} data={videos} renderItem={renderVideo} />
                 }
             />
-
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+    },
+    contentContainer: {
+        // paddingHorizontal: 8,
+        // paddingVertical: 4,
+    },
+    item: {
+        padding: 0,
+        margin: 10
+    },
+});
 
 export default RecordingsTab;
