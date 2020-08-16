e68069c14245dfb2e30df2e88dc03031bacaab45
Rishi <ushir.raval@gmail.com>
2020-08-16 13:06:06 +0200

    Connected to remote debugging and created generic Axios client
    Axios client will be used for Image Upload

--- WatchdogApp/app-redux/store.js
+++ WatchdogApp/app-redux/store.js
@@ -1,20 +1,36 @@
 import thunkMiddleware from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
-import watchdogApp from './reducer';
+import { createStore, applyMiddleware, compose } from 'redux';
 import axios from 'axios';
-import axiosMiddleware from 'redux-axios-middleware';
+import { createLogger } from 'redux-logger'
+import { multiClientMiddleware } from 'redux-axios-middleware';
+import watchdogApp from './reducer';
 
-const client = axios.create({ //all axios can be used, shown in axios documentation
+const apiClient = axios.create({ //all axios can be used, shown in axios documentation
     baseURL: 'https://b534kvo5c6.execute-api.af-south-1.amazonaws.com/testing/',
     responseType: 'json'
 });
 
+const genericClient = axios.create();
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const logger = createLogger({})
 const store = createStore(
     watchdogApp,
-    applyMiddleware(
-        thunkMiddleware,
-        axiosMiddleware(client)
+    composeEnhancers(
+        applyMiddleware(
+            thunkMiddleware,
+            multiClientMiddleware(
+                {
+                    default: {client: apiClient},
+                    generic: {client: genericClient}
+                }
+            ),
+            logger
+        )
     )
+
 )
 
 export default store;
