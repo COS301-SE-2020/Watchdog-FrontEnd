f1cad3fa1ad8ce8b96bb601b4b1beef1b40612a5
Luqmaan <luqmaan.badat@gmail.com>
2020-08-26 22:09:39 +0200

    set up prime react

---
+++ watchdog-web/pages/index.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+import 'primereact/resources/themes/nova/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+const IndexPage = () => (
+  <div >
+    <h1>Hello Next.js 👋</h1>
+    <p>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </p>
+  </div>
+)
+
+export default IndexPage
