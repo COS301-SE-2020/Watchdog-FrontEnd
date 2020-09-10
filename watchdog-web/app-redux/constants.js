a6ef0da80069f382209a5524262a8f2b4c487da9
Rishi <ushir.raval@gmail.com>
2020-09-10 02:08:09 +0200

    Changed ec2 url and default state type

--- watchdog-web/app-redux/constants.js
+++ watchdog-web/app-redux/constants.js
@@ -1,2 +1,3 @@
-export const LIVE_SERVER_URL = 'http://ec2-13-245-14-169.af-south-1.compute.amazonaws.com:8080'
+// export const LIVE_SERVER_URL = 'https://ec2-13-244-153-139.af-south-1.compute.amazonaws.com/'
+export const LIVE_SERVER_URL = 'http://ec2-13-244-153-139.af-south-1.compute.amazonaws.com:8080/'
 export const API_BASE_URL = 'https://b534kvo5c6.execute-api.af-south-1.amazonaws.com/testing/'
