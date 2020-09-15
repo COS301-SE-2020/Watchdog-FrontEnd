f862e9dfb8ee603894dd8173dd5e1020b3c980df
Abubakr <abubakrkharbai1@gmail.com>
2020-09-15 12:34:18 +0200

    Adedd Cypress to esting Enviroment

---
+++ watchdog-web/cypress/integration/examples/watchdog.js
@@ -0,0 +1,5 @@
+describe('Accessing Watchdog System test', function(){
+    it('Visits the Deployed Watchdog System', function(){
+        cy.visit('https://lynksolutions.watchdog.thematthew.me')
+    })
+})
