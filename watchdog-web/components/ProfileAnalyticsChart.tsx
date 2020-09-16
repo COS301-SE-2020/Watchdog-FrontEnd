f7d74867c806a822a4d2a47068b2cabf11f87e87
Luqmaan <luqmaan.badat@gmail.com>
2020-09-16 14:30:48 +0200

    allow filtering of graph data

--- watchdog-web/components/ProfileAnalyticsChart.tsx
+++ watchdog-web/components/ProfileAnalyticsChart.tsx
@@ -4,11 +4,8 @@ import { getProfileAnalytics } from '../api'
 import ProfileAnalyticModals from './ProfileAnalyticModals'
 import { Dropdown } from 'primereact/dropdown'
 import { runInThisContext } from 'vm'
-// make sure parent container have a defined height when using
-// responsive component, otherwise height will be 0 and
-// no chart will be rendered.
-// website examples showcase many properties,
-// you'll often use just a few of them.
+import LoadingOverlay from 'react-loading-overlay'
+import MoonLoader from 'react-spinners/MoonLoader'
 
 
 interface ProfileAnalyticsProps {
@@ -18,11 +15,15 @@ interface ProfileAnalyticsProps {
 }
 interface ProfileAnalyticsState {
     data: any[]
+    display_data: any[]
     modal: boolean
     name: string
     img: any[]
     scale: string
     prev_scale: string
+    profiles: []
+    profiles_display: []
+    loading: boolean
 
 }
 
@@ -38,13 +39,18 @@ class ProfileAnalyticsChart extends Component<ProfileAnalyticsProps, ProfileAnal
             name: '',
             img: [],
             scale: 'WEEKLY',
-            prev_scale: ''
+            prev_scale: '',
+            profiles: [],
+            profiles_display: [],
+            display_data: [],
+            loading: false
 
         }
 
         this.onClickDatapoint = this.onClickDatapoint.bind(this)
         this.toggleModal = this.toggleModal.bind(this)
         this.getData = this.getData.bind(this)
+        this.applyFilter = this.applyFilter.bind(this)
 
     }
 
@@ -73,11 +79,67 @@ class ProfileAnalyticsChart extends Component<ProfileAnalyticsProps, ProfileAnal
 
     }
 
+    async applyFilter(val) {
+        //console.log(val)
+        this.setState({loading: true})
+        let array = this.state.profiles_display
+        if (this.state.profiles_display.includes(val)) {
+            //console.log()
+
+            let index = array.indexOf(val)
+            array.splice(index, 1)
+            console.log(array)
+            this.setState({ profiles_display: array })
+
+
+        } else {
+
+            array.push(val)
+            this.setState({ profiles_display: array })
+            console.log(array)
+        }
+        //this.setState({display_data: this.state.data})
+        await getProfileAnalytics(this.state.scale, async (e) => {
+
+
+            await this.setState({ data: e.data.data, prev_scale: this.state.scale })
+
+        }, () => {
+
+        })
+        let displayData = this.state.data.map((item) => {
+            let toReturn = item
+            console.log(toReturn)
+            if (!array.includes(item.id)) {
+                console.log(item.id)
+                toReturn.data = []
+
+            }
+            console.log(toReturn)
+            return (toReturn)
+        })
+
+
+
+
+        this.setState({ display_data: displayData })
+        this.setState({loading: false})
+
+
+    }
+
     getData() {
+        let array = []
         if (this.state.prev_scale !== this.state.scale) {
             getProfileAnalytics(this.state.scale, (e) => {
+                console.log(e.data.data)
+                e.data.data.forEach(element => {
+                    array.push(element.id)
 
-                this.setState({ data: e.data.data, prev_scale: this.state.scale })
+
+                })
+
+                this.setState({ data: e.data.data, prev_scale: this.state.scale, profiles: array, profiles_display: array, display_data: e.data.data })
 
             }, () => {
 
@@ -86,107 +148,118 @@ class ProfileAnalyticsChart extends Component<ProfileAnalyticsProps, ProfileAnal
     }
 
     componentDidUpdate() {
-        this.getData()
+        //this.getData()
     }
 
 
 
 
     componentDidMount() {
+        this.getData()
 
-        getProfileAnalytics(this.state.scale, (e) => {
+        // getProfileAnalytics(this.state.scale, (e) => {
 
 
-            this.setState({ data: e.data.data })
+        //     this.setState({ data: e.data.data })
 
-        }, () => {
+        // }, () => {
 
-        })
+        // })
 
     }
 
     render() {
         return (
-            <div style={{ height: this.props.height }}>
+            <LoadingOverlay
+                active={this.state.loading}
+                spinner={<MoonLoader color={'#25b3f5'} />}
+                style={{ wrapper: { width: '100vw', height: '100vh' } }}
 
-                <ProfileAnalyticModals name={this.state.name} img_list={this.state.img} show_modal={this.state.modal} hide_modal={this.toggleModal} />
-                <ResponsiveLine
+            >
+                <div style={{ height: this.props.height }}>
 
-                    onClick={this.onClickDatapoint}
-                    data={this.state.data}
-                    margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-                    curve='linear'
-                    xScale={{ type: 'point' }}
-                    yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: false, reverse: false }}
-                    axisTop={null}
-                    axisRight={null}
-                    axisBottom={{
-                        orient: 'bottom',
-                        tickSize: 5,
-                        tickPadding: 5,
-                        tickRotation: 0,
-                        legend: 'Date',
-                        legendOffset: 36,
-                        legendPosition: 'middle'
-                    }}
-                    axisLeft={{
-                        orient: 'left',
-                        tickSize: 5,
-                        tickPadding: 5,
-                        tickRotation: 0,
-                        legend: 'count',
-                        legendOffset: -40,
-                        legendPosition: 'middle'
-                    }}
-                    colors={{ scheme: 'nivo' }}
-                    pointSize={10}
-                    pointColor={{ theme: 'background' }}
-                    pointBorderWidth={2}
-                    pointBorderColor={{
-                        from: 'serieColor'
-                    }}
+                    <ProfileAnalyticModals name={this.state.name} img_list={this.state.img} show_modal={this.state.modal} hide_modal={this.toggleModal} />
+                    <ResponsiveLine
 
-                    pointLabel="Date"
-                    pointLabelYOffset={-12}
-                    useMesh={true}
-                    legends={[
-                        {
-                            itemTextColor: 'white',
-                            anchor: 'bottom-right',
-                            direction: 'column',
-                            justify: false,
-                            translateX: 100,
-                            translateY: 0,
-                            itemsSpacing: 0,
-                            itemDirection: 'left-to-right',
-                            itemWidth: 80,
-                            itemHeight: 20,
-                            itemOpacity: 1,
-                            symbolSize: 12,
-                            symbolShape: 'circle',
-                            symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                            effects: [
-                                {
-                                    on: 'hover',
-                                    style: {
-                                        itemBackground: 'rgba(0, 0, 0, 1)',
-                                        itemOpacity: 1
+                        onClick={this.onClickDatapoint}
+                        data={this.state.display_data}
+                        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+                        curve='linear'
+                        xScale={{ type: 'point' }}
+                        yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: false, reverse: false }}
+                        axisTop={null}
+                        axisRight={null}
+                        axisBottom={{
+                            orient: 'bottom',
+                            tickSize: 5,
+                            tickPadding: 5,
+                            tickRotation: 0,
+                            legend: 'Date',
+                            legendOffset: 36,
+                            legendPosition: 'middle'
+                        }}
+                        axisLeft={{
+                            orient: 'left',
+                            tickSize: 5,
+                            tickPadding: 5,
+                            tickRotation: 0,
+                            legend: 'count',
+                            legendOffset: -40,
+                            legendPosition: 'middle'
+                        }}
+                        colors={{ scheme: 'nivo' }}
+                        pointSize={10}
+                        pointColor={{ theme: 'background' }}
+                        pointBorderWidth={2}
+                        pointBorderColor={{
+                            from: 'serieColor'
+                        }}
+
+                        pointLabel="Date"
+                        pointLabelYOffset={-12}
+                        useMesh={true}
+                        legends={[
+                            {
+                                itemTextColor: 'white',
+                                anchor: 'bottom-right',
+                                direction: 'column',
+                                justify: false,
+                                translateX: 100,
+                                translateY: 0,
+                                itemsSpacing: 0,
+                                itemDirection: 'left-to-right',
+                                itemWidth: 80,
+                                itemHeight: 20,
+                                itemOpacity: 1,
+                                symbolSize: 12,
+                                symbolShape: 'circle',
+                                symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                                effects: [
+                                    {
+                                        on: 'hover',
+                                        style: {
+                                            itemBackground: 'rgba(0, 0, 0, 1)',
+                                            itemOpacity: 1
+                                        }
                                     }
+                                ],
+                                onClick: (e) => {
+                                    this.applyFilter(e.id)
+                                    //console.log(e.id)
                                 }
-                            ]
-                        }
-                    ]}
-                    isInteractive={true}
-                    tooltip={(e) => {
-                        console.log(e)
+                            }
+                        ]}
+                        isInteractive={true}
+                        tooltip={(e) => {
 
-                        return (<span style={{ backgroundColor: e.point.serieColor }} className="p-badge p-badge-warning">{e.point.data.y}</span>)
-                    }}
-                />
+                            return (<span style={{ backgroundColor: e.point.serieColor }} className="p-badge p-badge-warning">{e.point.data.y}</span>)
+                        }}
+                    />
 
 
 
-            </div>
+                </div>
+            </LoadingOverlay>
 
         )
 
